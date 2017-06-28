163655a12e1acbff6c61e5b90c316e301dac2cdd
Daniel Whilchy <dwhilchy@tycoint.com>
2017-06-28 22:40:20 +0100

    Initial commit with base application structure

---
+++ app/src/main/java/com/whilchy/roomsampleapp/ToDoActivity.kt
@@ -0,0 +1,12 @@
+package com.whilchy.roomsampleapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ToDoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do)
+    }
+}
